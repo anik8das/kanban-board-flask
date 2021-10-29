ccf2a3c5bbd93a0968dcc001fbecda0610b0374b
Aniket Das <aniketdas001@gmail.com>
2021-10-29 23:17:33 +0200

    features working!

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 db = {
-    "todo": [{"id": 1, "description": 'Falseyoo', "title": 'yo'}],
-    "doing": [{"id": 1, "description": 'Falseyoo', "title": 'yoo'}],
-    "complete": [{"id": 1, "description": 'Falseyoo', "title": 'yooo'}]
+    "todo": [{"description": 'Falseyoo', "title": 'yo'}],
+    "doing": [{"description": 'Falseyoo', "title": 'yoo'}],
+    "complete": [{"description": 'Falseyoo', "title": 'yooo'}]
 }
 
 app = Flask(__name__)
@@ -14,11 +14,24 @@ def index():
     print(db)
     return render_template('index.html', db=db)
 
+@app.route("/add/<string:cat>", methods=["POST"])
+def add(cat):
+    title = request.form.get("title")
+    description = request.form.get("description")
+    db[cat].append({'title': title, 'description': description})
+    return redirect(url_for("index"))
 
-@app.route("/add")
-def add():
-    return "<p>Hello, Index!</p>"
+@app.route("/delete/<string:cat>/<int:pos>")
+def delete(cat, pos):
+    db[cat].pop(pos)
+    return redirect(url_for("index"))
 
+@app.route("/move/<string:initCat>/<int:pos>", methods=["POST"])
+def move(initCat, pos):
+    print('move was called')
+    finalCat = request.form.get('finalCat')
+    db[finalCat].append(db[initCat].pop(pos))
+    return redirect(url_for("index"))
 
 if (__name__ == "__main__"):
     app.run(debug=True)
